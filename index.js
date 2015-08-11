bd0bfbe696fcd7738b39928e2fa3226db11c9ba3
Matt Edelman <medelman@paypal.com>
2015-08-11 16:40:57 -0700

    export moduleBuilder separately

--- index.js
+++ index.js
@@ -7,14 +7,19 @@ var path = require('path');
 var fs = require('fs');
 
 
-module.exports = function streamLocale(appRoot) {
+module.exports = function writeLocale(appRoot) {
     return function (locale, cb) {
         var m = /(.*)-(.*)/.exec(locale); // Use a real BCP47 parser.
         var outputRoot = path.resolve(appRoot, path.join('.build', locale));
-        mkdirp(outputRoot, iferr(cb, function (out) {
-            spundle(path.resolve(appRoot, 'locales'), m[2], m[1], iferr(cb, function (out) {
-                fs.writeFile(path.resolve(outputRoot, '_languagepack.js'), 'define("_languagepack", function () { return ' + JSON.stringify(out) + '; });', cb);
+        mkdirp(outputRoot, iferr(cb, function () {
+            moduleBuilder(appRoot, iferr(cb, function (out) {
+                fs.writeFile(path.resolve(outputRoot, '_languagepack.js'), out, cb);
             }));
         }));
     }
+};
+var moduleBuilder = module.exports.moduleBuilder = function (appRoot, cb) {
+    spundle(path.resolve(appRoot, 'locales'), m[2], m[1], iferr(cb, function (out) {
+        cb(null, 'define("_languagepack", function () { return ' + JSON.stringify(out) + '; });');
+    }));
 };
