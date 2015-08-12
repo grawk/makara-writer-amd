4eff79c2a0c8f3327c247324e89484f16f6ba4e0
Matt Edelman <medelman@paypal.com>
2015-08-11 18:25:02 -0700

    rename builder function

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ module.exports = function writeLocale(appRoot) {
         var m = /(.*)-(.*)/.exec(locale); // Use a real BCP47 parser.
         var outputRoot = path.resolve(appRoot, path.join('.build', locale));
         mkdirp(outputRoot, iferr(cb, function () {
-            moduleBuilder(appRoot, m, iferr(cb, function (out) {
+            amdBuilder(appRoot, m, iferr(cb, function (out) {
                 fs.writeFile(path.resolve(outputRoot, '_languagepack.js'), out, cb);
             }));
         }));
     }
 };
-var moduleBuilder = module.exports.moduleBuilder = function (appRoot, m, cb) {
+var amdBuilder = module.exports.amdBuilder = function (appRoot, m, cb) {
     spundle(path.resolve(appRoot, 'locales'), m[2], m[1], iferr(cb, function (out) {
         cb(null, 'define("_languagepack", function () { return ' + JSON.stringify(out) + '; });');
     }));
