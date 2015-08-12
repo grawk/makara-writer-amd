46be1cf8389c8de8f39050078214a3c83fb0816d
Matt Edelman <medelman@paypal.com>
2015-08-11 19:40:00 -0700

    pass in locale root directly

--- index.js
+++ index.js
@@ -11,15 +11,16 @@ module.exports = function writeLocale(appRoot) {
     return function (locale, cb) {
         var m = /(.*)-(.*)/.exec(locale); // Use a real BCP47 parser.
         var outputRoot = path.resolve(appRoot, path.join('.build', locale));
+        var localeRoot = path.resolve(appRoot, 'locales');
         mkdirp(outputRoot, iferr(cb, function () {
-            amdBuilder(appRoot, m, iferr(cb, function (out) {
+            amdBuilder(localeRoot, m, iferr(cb, function (out) {
                 fs.writeFile(path.resolve(outputRoot, '_languagepack.js'), out, cb);
             }));
         }));
     }
 };
-var amdBuilder = module.exports.amdBuilder = function (appRoot, m, cb) {
-    spundle(path.resolve(appRoot, 'locales'), m[2], m[1], iferr(cb, function (out) {
+var amdBuilder = module.exports.amdBuilder = function (localeRoot, m, cb) {
+    spundle(path.resolve(localeRoot, 'locales'), m[2], m[1], iferr(cb, function (out) {
         cb(null, 'define("_languagepack", function () { return ' + JSON.stringify(out) + '; });');
     }));
 };
